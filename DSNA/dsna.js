00abfd46a61054c8a1901791721359dfa475e08b
adria <littleivanitsky@gmail.com>
2022-04-04 11:23:12 -0700

    oddishOrEvening and at functions working

--- DSNA/dsna.js
+++ DSNA/dsna.js
@@ -21,3 +21,30 @@ function titleCase(sentence) {
 }
 
 // console.log(titleCase('aLcHeMy RoCkS gOlD'));
+
+//oddish vs evenish
+
+function oddishOrEvenish(number) {
+  const sumOfDigits = number
+    .toString()
+    .split('')
+    .map((digit) => +digit)
+    .reduce((sum, number) => sum + number);
+
+  return sumOfDigits % 2 ? 'Oddish' : 'Evenish';
+}
+
+console.log(oddishOrEvenish(121));
+console.log(oddishOrEvenish(41));
+
+// at
+
+function at(arr, index) {
+  if (index > -1) {
+    return arr[index];
+  } else {
+    return arr[arr.length + index];
+  }
+}
+console.log(at(['a', 'b', 'c', 'd', 'e'], 1));
+console.log(at(['a', 'b', 'c', 'd', 'e'], -2));
