1fc4bf472494b3b82f4f73fc458f85295fddcb49
adria <littleivanitsky@gmail.com>
2022-04-04 14:20:42 -0700

    struggling through unique string following along with dan

--- DSNA/dsna.js
+++ DSNA/dsna.js
@@ -34,8 +34,8 @@ function oddishOrEvenish(number) {
   return sumOfDigits % 2 ? 'Oddish' : 'Evenish';
 }
 
-console.log(oddishOrEvenish(121));
-console.log(oddishOrEvenish(41));
+// console.log(oddishOrEvenish(121));
+// console.log(oddishOrEvenish(41));
 
 // at
 
@@ -46,5 +46,58 @@ function at(arr, index) {
     return arr[arr.length + index];
   }
 }
-console.log(at(['a', 'b', 'c', 'd', 'e'], 1));
-console.log(at(['a', 'b', 'c', 'd', 'e'], -2));
+// console.log(at(['a', 'b', 'c', 'd', 'e'], 1));
+// console.log(at(['a', 'b', 'c', 'd', 'e'], -2));
+
+// sorting
+
+['a', 'b', 'Aa', 'AA', 'bB', 'bb'].sort();
+[1, 2, 10, 50, 500, 90].sort();
+
+//remove duplicates
+
+const numbers = [1, 1, 1, 2, 3, 4];
+const unique = new Set(numbers);
+const uniqueArray = [...unique];
+
+//fizz buzz
+
+function fizzBuzz(number) {
+  const list = [];
+
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      list.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      list.push('Fizz');
+    } else if (i % 5 === 0) {
+      list.push('Buzz');
+    } else {
+      list.push(i);
+    }
+  }
+  return list;
+}
+// console.log(fizzBuzz(16));
+// anagrams
+
+function anagrams(wordOne, wordTwo) {
+  const first = wordOne.split('').sort().join('');
+  const second = wordTwo.split('').sort().join('');
+
+  return first === second;
+}
+console.log(anagrams('pictorialness', 'documentarily'));
+
+// unique string
+
+function uniqueString(strings) {
+  const orderedUniqueLetters = strings.map((s) => [...new Set(s)].sort());
+  const duplicates = [...new Set(...orderedUniqueLetters)];
+
+  return strings.find(string) => {
+    return duplicates.indexOf(string[0]) === -1;
+
+  }
+}
+console.log(uniqueString(['abc', 'acb', 'bac', 'foo', 'bar']))
